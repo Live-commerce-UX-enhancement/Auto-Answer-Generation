57421c34519f3f48691a67a21fc719a8a093ff75
yuseogi0218 <yuseogi0218@gmail.com>
2023-09-18 14:10:16 +0900

    Classifier test

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 class BroadcastInformation(BaseModel):
     type: str
     texts: List[str]
@@ -46,3 +47,43 @@ def get_answer(broadcast_id, q: str):
 @app.get("/ping")
 def pingPong():
     return "pong"
+
+
+class Chat(BaseModel):
+    commentType: str
+    idNo: str
+    commentNo: int
+    nickname: str
+    message: str
+
+class Item(BaseModel):
+    list: List[Chat]
+    next: int
+
+idx = 0
+@app.post("/classifier_api/classify")
+def classify(item : Item):
+    global idx
+    chat_list = item.list
+
+    result = dict()
+    result["chat_data"] = list()
+
+    for chat_idx in range(len(chat_list)):
+        idx += 1
+        chat = chat_list[chat_idx]
+        classifier_result = dict()
+        classifier_result["commentNo"] = chat.commentNo
+        classifier_result["nickname"] = chat.nickname
+        classifier_result["message"] = chat.message
+
+        if idx % 3 == 0:
+            classifier_result["result"] = "일반"
+        elif idx % 3 == 1:
+            classifier_result["result"] = "질문"
+        else:
+            classifier_result["result"] = "요청"
+
+        result["chat_data"].append(classifier_result)
+
+    return result
