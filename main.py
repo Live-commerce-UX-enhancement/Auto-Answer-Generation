842165285a548d3973abd9e5e5ac076a3fb1a902
yuseogi0218 <yuseogi0218@gmail.com>
2023-10-28 16:49:17 +0900

    websocket test

--- main.py
+++ main.py
@@ -99,10 +99,13 @@ async def websocket_endpoint(websocket: WebSocket, room_id: str):
     try:
         while True:
             data = await websocket.receive_text()
+
+            # vector store 에 답변 정보 추가
+
             # 메시지를 받았을 때 모든 연결된 클라이언트에게 broadcast 합니다.
             for client in connected_room[room_id]:
                 await client.send_text(data)
     except WebSocketDisconnect:
         # 연결이 닫힌 경우 클라이언트를 connected_clients에서 제거합니다.
-        await websocket.close()
+        # await websocket.close()
         connected_room[room_id].remove(websocket)
